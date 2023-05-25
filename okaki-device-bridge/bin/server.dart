e7c0c6ee6890dcc94a536f7d2a1494ae39f52821
renebohne <rene.bohne@gmail.com>
2023-05-25 14:09:54 +0200

    default port 8079

--- okaki-device-bridge/bin/server.dart
+++ okaki-device-bridge/bin/server.dart
@@ -95,7 +95,7 @@ void main(List<String> args) async {
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
 
   // For running in containers, we respect the PORT environment variable.
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final port = int.parse(Platform.environment['PORT'] ?? '8079');
   final server = await serve(handler, ip, port);
   print('Server listening on port ${server.port}');
 }
